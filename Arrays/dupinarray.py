f911919c1d1e0e693fd51b19d97e7d63aa3da2a3
sanindra <sanindrakumar@gmail.com>
2017-07-29 02:36:11 +0530

    find a duplicate item in an array

---
+++ Arrays/dupinarray.py
@@ -0,0 +1,17 @@
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def repeatedNumber(self, A):
+        A = list(A)
+        for i in xrange(len(A)):
+            x = abs(A[i])-1
+            if A[x]>0:
+                A[x] *=-1
+            else:
+                return abs(A[i])
+        return -1
+
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.repeatedNumber([3, 1, 1, 2])
