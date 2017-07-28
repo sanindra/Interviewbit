00d69b58c4e780cf263418fb3918065ead788fb1
sanindra <sanindrakumar@gmail.com>
2017-07-29 02:37:28 +0530

    min steps to traverse an infinte grid

---
+++ Arrays/minstepsinfintegrid.py
@@ -0,0 +1,21 @@
+
+
+class Solution:
+    # @param X : list of integers
+    # @param Y : list of integers
+    # Points are represented by (X[i], Y[i])
+    # @return an integer
+    import math
+    def coverPoints(self, X, Y):
+        a, b = X[0], Y[0]
+        c = 0
+        for i in range(1, len(X)):
+            c += max(abs(X[i]-a), abs(Y[i]-b))
+            a, b = X[i], Y[i]
+            
+        return int(c)
+
+
+if __name__ == '__main__':
+	sol = Solution()
+	print sol.coverPoints([0, 1, 1], [0, 1, 2])
