11ceced7aafea2e63f81217c1474779a389e0f3b
sanindra <sanindrakumar@gmail.com>
2017-07-30 19:54:23 +0530

    find missing and repeated numbers in an array

---
+++ Arrays/repeatnumber.py
@@ -0,0 +1,19 @@
+class Solution:
+    # @param A : tuple of integers
+    # @return a list of integers
+    def repeatedNumber(self, A):
+    	l, A = len(A), list(A)
+    	y = list(set(xrange(1, l+1)) - set(A))
+
+    	for i in xrange(l):
+    		x = abs(A[i]) - 1
+    		if A[x] >0:
+    			A[x] *= -1
+    		else:
+    			y.insert(0, abs(A[i]))
+    			break
+    	return y
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.repeatedNumber([3, 1, 1, 2])
