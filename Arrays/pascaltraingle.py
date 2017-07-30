f28c6feac316170719f765b42a6dbf322622e34d
sanindra <sanindrakumar@gmail.com>
2017-07-31 00:54:52 +0530

    pascal triangle

---
+++ Arrays/pascaltraingle.py
@@ -0,0 +1,18 @@
+class Solution:
+    # @param A : integer
+    # @return a list of list of integers
+    def generate(self, A):
+        return [self.generateRows(i) for i in xrange(A)]
+    
+    def generateRows(self, n):
+        row = [0 for i in xrange(n+1)]
+        row[0], row[-1] = 1, 1
+        
+        for i in xrange(n/2):
+            y = row[i]*(n-i)/(i+1)
+            row[i+1], row[n-i-1] = y, y
+        return row
+
+if __name__ == '__main__':
+    sol = Solution()
+    print sol.generate(5)
